8ca1bfae50128042d780f5f360abc8873c5b9f3b
Luka Kramer <luka@ichbinich.ch>
2019-09-27 11:18:04 +0200

    setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import {createClient} from 'contentful-management';
+
+
+
+window.contentfulClient = createClient({
+  accessToken: 'CFPAT-RwIXTc0puQNDc3v1EyUye59pEOePrZopcR6moMAZEhc'
+});
+
+window.contentfulClient.getSpace('cse4rkqqs7sp')
+  .then((space) => space.getEnvironment('master'))
+  .then((environment) => environment.getEntries())
+  .then((response) => console.log(response.items))
+  .catch(console.error);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <div id="map"></div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
+<style scoped lang="scss">
+  #map {
+    height: 80vh;
+    width: 80vw;
+  }
+</style>
 
 <script>
 // @ is an alias to /src
@@ -13,6 +19,14 @@ export default {
   name: "home",
   components: {
     HelloWorld
+  },
+  mounted: function(){
+    const element = document.getElementById("map")
+    const options = {
+      zoom: 14,
+      center: new google.maps.LatLng(47.071467, 8.277621)
+    }
+    this.map = new google.maps.Map(element, options);
   }
 };
 </script>
