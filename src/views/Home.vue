c53f80c564ee85eb821011788e5c5731aacb2c60
Dariush <dariush007@gmx.ch>
2019-10-03 11:25:06 +0200

    Blabla 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,26 +7,28 @@
         </ul>
     </div>
 </template>
-
+<style lang="scss">
+    @import '../styles/home.sass';
+</style>
 <script>
   //  import Vue from "Vue";
   export default {
     name: "home",
-    data: function() {
-      return {
-        regionen: [
-          { id:"2",name: "Luzern" },
-          { id:"3",name: "Bern" },
-          { id:"4",name: "Basel" },
-          { id:"5",name: "St.Gallen" },
-          { id:"6",name: "Zürich" }
-        ]
-      };
-    }
-  ,
-  components: {
-  }
-  ,
+    data:
+      function() {
+        return {
+          regionen: [
+            { id: "2", name: "Luzern" },
+            { id: "3", name: "Bern" },
+            { id: "4", name: "Basel" },
+            { id: "5", name: "St.Gallen" },
+            { id: "6", name: "Zürich" }
+          ]
+        };
+      }
+    ,
+    components: {}
+
 
   }
   ;
